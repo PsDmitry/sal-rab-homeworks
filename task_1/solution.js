cb5bab8cbf185d2ed2caa0d3b9301d612a8a031f
PsDmitry <irkzhlbvf27@gmail.com>
2022-11-18 14:06:27 +0300

    home 1.3

--- task_1/solution.js
+++ task_1/solution.js
@@ -2,7 +2,6 @@ function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
 
-    // Задание №1.1. Оповещение о добавлении в корзину
 
     let message = productName;
     message = message + " ";
@@ -18,16 +17,11 @@ function addInCartMessage(title, price) {
 
 function addInCartCountChange(value) {
 
-    // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
 
-    // создайте переменную newValue
     let newValue;
-    // присвойте ей значение oldValue
     newValue = oldValue
-    // увеличьте значение newValue на 1
     newValue =  newValue + 1
-    // Конец решения задания №1.2.
 
     return newValue;
 }
@@ -36,13 +30,12 @@ function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
 
-    // Задание №1.3. Увеличение суммы в корзине
 
-    // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
-
-    // Конец решения задания №1.3.
+    let newSum
+    newSum = oldSum + difference;
+    let newSumText;
+    newSumText = newSum + '₽';
+    newSumText = `${newSum} ₽`
 
     return newSumText;
 }
